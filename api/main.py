1d650f5c801a42dd7a7aeb9330a37054aa3f238d
abdulamitovshaxbozbek-dot <abdulamitovshaxbozbek@gmail.com>
2026-09-08 01:27:17 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from database import get_connection
 
 app = FastAPI()
 
@@ -6,3 +7,16 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+@app.get("/health/db")
+def health_db():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT 1")
+            result = cur.fetchone()
+
+    return {
+        "status": "ok",
+        "database": result[0] == 1
+    }
